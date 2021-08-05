8a2812076e7654d890d3592a93b919e041d7a54f
Gonenc Filiz <gonenc@dakikyazilim.com>
2021-08-05 12:09:03 +0300

    Created basic "Task" component with storybook.

---
+++ src/components/Task.vue
@@ -0,0 +1,50 @@
+<template>
+  <div :class="classes">
+    <label class="checkbox">
+      <input type="checkbox" :checked="isChecked" disabled name="checked"/>
+      <span class="checkbox-custom" @click="archiveText"/>
+    </label>
+    <div class="title">
+      <input type="text" :value="task.title" readonly placeholder="Input Title"/>
+    </div>
+    <div class="actions">
+      <a v-if="!isChecked" @click="pinTask">
+        <span class="icon-star"/>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+import {reactive, computed} from "vue";
+
+export default {
+  name: 'Task',
+  props: {
+    task: {
+      type: Object,
+      required: true,
+      default: () => ({id: '', state: '', title: ''}),
+      validator: task => ['id', 'state', 'title'].every(key => key in task),
+    },
+  },
+  emits: ['archive-task', 'pin-task'],
+  setup(props, {emit}) {
+    props = reactive(props);
+    return {
+      classes: computed(() => ({
+        'list-item TASK_INBOX': props.task.state === 'TASK_INBOX',
+        'list-item TASK_PINNED': props.task.state === 'TASK_PINNED',
+        'list-item TASK_ARCHIVED': props.task.state === 'TASK_ARCHIVED',
+      })),
+      isChecked: computed(() => props.task.state === 'TASK_ARCHIVED'),
+      archiveTask() {
+        emit('archive-task', props.task.id);
+      },
+      pinTask() {
+        emit('pin-task', props.task.id);
+      },
+    }
+  }
+};
+</script>
